0df5953ce091732aab187578680f7d3f2c49e11c
Edgar HiSo <edghiso@gmail.com>
2022-09-17 13:37:38 +0200

    feat: resolve api

--- api/config/routes.config.js
+++ api/config/routes.config.js
@@ -2,6 +2,10 @@ const express = require('express');
 const router = express.Router();
 const streams = require('../controllers/streams.controller');
 
-router.get('/streams', streams.list);
+router.get('/streams/', streams.list);
+router.post('/streams', streams.create);
+router.get('/streams/:id', streams.detail);
+router.put('/streams/:id', streams.update);
+router.delete('/streams/:id', streams.delete);
 
 module.exports = router;
