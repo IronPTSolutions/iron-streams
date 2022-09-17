8e860b5fabad42821e037c11e6566b851d0ab22b
Julio Garcia Perez <julio@local>
2022-09-17 09:41:45 +0200

    add model

--- api/models/stream.model.js
+++ api/models/stream.model.js
@@ -1,14 +1,30 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
-const streamSchema = new Schema({
-  title: {
-    type: String,
-    required: 'Title is required',
-    trim: true,
-    maxLength: [20, `Title must be <= 20 chars`]
-  }
-}, { timestamps: true });
+// Fields:
+//   title: String
+//   author: String (de momento)
+//   url: String
+//   description: String
+//   views: Number
+//   createdAt: Date *
+//   updatedAt: Date *
+//   category: String
+//   duration: Number
+//   thumbnail: String
+//   private: Boolean
 
-const Stream = mongoose.model('Stream', streamSchema);
+const streamSchema = new Schema(
+  {
+    title: {
+      type: String,
+      required: "Title is required",
+      trim: true,
+      maxLength: [20, `Title must be <= 20 chars`],
+    },
+  },
+  { timestamps: true }
+);
+
+const Stream = mongoose.model("Stream", streamSchema);
 module.exports = Stream;
