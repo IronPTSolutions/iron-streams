197da4c673d4ef8c577468250a76940cce32b9bf
Julio Garcia Perez <julio@local>
2022-09-17 11:04:14 +0200

    seeds

--- api/bin/seeds.js
+++ api/bin/seeds.js
@@ -1,10 +1,27 @@
 require("../config/db.config");
 
 const Stream = require("../models/stream.model");
+const { faker } = require("@faker-js/faker");
 
 Stream.deleteMany({})
   .then(() => {
-    // TODO: create 20 Streams using https://fakerjs.dev/guide/
+    for (let i = 0; i < 20; i++) {
+      Stream.create({
+        title: faker.music.songName(),
+        description: faker.lorem.paragraph(),
+        author: faker.internet.userName(),
+        url: faker.internet.url(),
+        views: Math.floor(Math.random() * 10000),
+        category: faker.music.genre(),
+        duration: Math.floor(Math.random() * 200),
+        thumbnail: faker.image.abstract(undefined, undefined, true),
+        private: Math.random() < 0.8,
+      }).then((stream) => {
+        console.log(`stream ${stream.title} created`);
+
+        // TODO: create relations
+      });
+    }
   })
   .catch((err) => {
     console.error(err);
--- api/models/stream.model.js
+++ api/models/stream.model.js
@@ -1,26 +1,46 @@
 const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
-// Fields:
-//   title: String
-//   author: String (de momento)
-//   url: String
-//   description: String
-//   views: Number
-//   createdAt: Date *
-//   updatedAt: Date *
-//   category: String
-//   duration: Number
-//   thumbnail: String
-//   private: Boolean
-
 const streamSchema = new Schema(
   {
     title: {
       type: String,
       required: "Title is required",
       trim: true,
-      maxLength: [20, `Title must be <= 20 chars`],
+      maxLength: [100, `Title must be <= 100 chars`],
+    },
+    description: {
+      type: String,
+      required: "Description is required",
+    },
+    author: {
+      type: String,
+      required: "Author is required",
+      trim: true,
+    },
+    url: {
+      type: String,
+      required: "Url is required",
+      trim: true,
+    },
+    views: {
+      type: Number,
+    },
+    category: {
+      type: String,
+      required: "Category is required",
+      trim: true,
+    },
+    duration: {
+      type: Number,
+    },
+    thumbnail: {
+      type: String,
+      required: "Thumbnail is required",
+      trim: true,
+    },
+    private: {
+      type: Boolean,
     },
   },
   { timestamps: true }
