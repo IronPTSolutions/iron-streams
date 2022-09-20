2c8048ef27298d30041890edf18e76f58bff5fd1
Julio Garcia Perez <julio@local>
2022-09-20 19:55:47 +0200

    axios and stream service

---
+++ web/src/services/stream-service.js
@@ -0,0 +1,13 @@
+import axios from "axios";
+
+const http = axios.create({
+  baseURL: "http://localhost:3001/api/v1/streams",
+});
+
+export function getStreams() {
+  // TODO
+}
+
+export function getStream(id) {
+  // TODO
+}
